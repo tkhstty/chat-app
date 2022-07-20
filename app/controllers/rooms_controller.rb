ccea254bf7f6df0ae376344e17ac2f6699cc2d39
tkhstty <tkhstty@gmail.com>
2022-07-20 17:55:36 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,11 +4,23 @@ class RoomsController < ApplicationController
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+
+  private
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
   end
 end

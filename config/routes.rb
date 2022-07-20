104b1b2b6a46520e372d76d112bb797219b57322
tkhstty <tkhstty@gmail.com>
2022-07-20 21:53:04 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:index, :new, :create, :show, :destroy]
+  resources :rooms, only: [:new, :create, :destroy] do
+    resources :messages, only: [:index, :create, :destroy]
+  end
 end

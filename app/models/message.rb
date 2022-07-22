930a8c49ea2ac0ed3d184202a4db83ddea1638e1
tkhstty <tkhstty@gmail.com>
2022-07-22 14:30:38 +0900

    Revert "画像のみでもメッセージ送信可能となるバリデーションの設定"
    This reverts commit 068d6722bf347de119b392ca782a7260bce20940.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,11 +2,5 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
-  validates :content, presence: true, unless: :was_attached?
-
-
-  private
-  def was_attached?
-    self.image.attached?
-  end
+  validates :content, presence: true
 end
